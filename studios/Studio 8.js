e1a4315d28defd17fb3d8f9aaf8a859637d11677
Source Academy User <No public email provided>
2023-10-10 11:29:57 +0800

    Changes made from Source Academy

---
+++ studios/Studio 8.js
@@ -0,0 +1,43 @@
+// Studio 8
+
+// Qn 1
+1 + (2 + (3 + (4 + (5 + 6)))); // (a) 
+((((1 + 2) + 3) + 4) + 5) + 6; // (b)
+(1 + 2 + 3) + (4 + 5 + 6); // (c) 
+(1 + 2) + (3 + 4) + (5 + 6); // (d) 
+
+
+// Qn 2
+const c = 1;
+let x = c + 2;
+const d = c + x;
+let y = 3;
+x = c;
+y = d;
+
+
+// Qn 3
+1 + 2;
+const x = 1;
+3 * 4;
+let y = x - 7;
+
+
+// Qn 4
+while (false) {
+    1 + 2;
+}
+
+
+// Qn 5
+let x = 1;
+while (x < 10) {
+    x = x * 2;
+}
+
+
+// Qn 6
+let x = undefined;
+for (x = 0; x < 10; x = x + 2) {
+    x;
+}
