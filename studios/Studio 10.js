d6bfc4bcc5c3d391f1b816e12fcb7e713c3f9192
Source Academy User <No public email provided>
2023-10-24 11:29:51 +0800

    Changes made from Source Academy

--- studios/Studio 10.js
+++ studios/Studio 10.js
@@ -31,3 +31,46 @@ function bubblesort_list(L) {
 const LL = list(3, 5, 2, 4, 1);
 bubblesort_list(LL);
 LL; // should show [1, [2, [3, [4, [5, null]]]]]
+
+
+// Qn 3 memoization (incomplete)
+function cc(amount, kinds_of_coins) {
+    return amount === 0
+    ? 1
+    : amount < 0 || kinds_of_coins === 0
+    ? 0
+    : cc(amount, kinds_of_coins - 1)
+    +
+    cc(amount - first_denomination(kinds_of_coins),
+    kinds_of_coins);
+}
+function first_denomination(kinds_of_coins) {
+    return kinds_of_coins === 1 ? 5 :
+    kinds_of_coins === 2 ? 10 :
+    kinds_of_coins === 3 ? 20 :
+    kinds_of_coins === 4 ? 50 :
+    kinds_of_coins === 5 ? 100 : 0;
+}
+let A = []
+function write_in_mem(n, k, value) {
+    if (mem[n] === undefined) {
+        mem[n] = [];
+    }
+    mem[n][k] = value;
+}
+function read_in_mem(n, k) {
+    return mem[n] === undefined
+           ? undefined
+           : mem[n][k];
+}
+function cc_memoize(amount, kinds_of_coins) {
+    if (amount >= && kinds_of_coins >= 0 && read_in_mem(amount, kinds_of_coins) !== undefined)
+    return amount === 0
+    ? 1
+    : amount < 0 || kinds_of_coins === 0
+    ? 0
+    : cc(amount, kinds_of_coins - 1)
+    +
+    cc(amount - first_denomination(kinds_of_coins),
+    kinds_of_coins);
+}
