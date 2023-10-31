6fe8ab83aabb60175ee7e19a15415f53f3c1e0c9
Source Academy User <No public email provided>
2023-10-31 10:27:47 +0800

    Changes made from Source Academy

---
+++ studios/Studio 11.js
@@ -0,0 +1,17 @@
+// Studio 10
+
+// Qn 1
+function scale_stream(c, stream) {
+    return stream_map(x => c * x, stream);
+}
+const A = pair(1, () => scale_stream(2, A));
+// returns 1, 2, 4, 8, 16 keeps scaling by 2
+function mul_streams(a,b) {
+    return pair(head(a) * head(b),
+            () => mul_streams(stream_tail(a), stream_tail(b)));
+}
+const integers = integers_from(1);
+const B = pair(1, () => mul_streams(B, integers));
+// returns 1, 1, 2, 6, 24, 120, 6!, 7!, 8!, 9! and so on
+
+eval_stream(B, 10);
